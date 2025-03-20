fa0bc156bb8d691365ced1ecbac0ffa0785b0cbd
jl5047537 <jl5047537@gmail.com>
2025-03-20 16:18:22 +0300

    Обновлено имя бота в исходном и скомпилированном файлах

--- dist/lib/utils/consts.js
+++ dist/lib/utils/consts.js
@@ -1,3 +1,3 @@
 export var WALLET_MANIFEST_URL = 'https://crimson-magnetic-marsupial-746.mypinata.cloud/ipfs/bafkreihnpax4buvmwisnhgkdkb7v6mazgqg2ohwl4hfgdw2wddb7gzbbr4';
 export var LEVEL_START = 5;
-export var BOT_NAME = "RuZaAuthBot";
+export var BOT_NAME = "TestKassaTestBot";
